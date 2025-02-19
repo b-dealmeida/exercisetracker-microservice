1b286161c50203f4fb8e0dbbc4250f337d901197
Brandon DeAlmeida <58648733+b-dealmeida@users.noreply.github.com>
2025-02-19 12:46:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
 app.use(express.urlencoded({extended: true}));
 
 users = []
@@ -17,29 +16,14 @@ exercises = []
 
 // Allow the creation of a new user
 app.post('/api/users', (req, res) => {
-  // Ensure username isn't already taken
-  let usernameTaken = false
-  users.forEach(user => {
-    if(user['username'] === req.body['username']) {      
-      usernameTaken = true
-      return
-    }
-  })
-
-  // Return error if username taken
-  if(usernameTaken) {
-    res.json({'error': 'Username already exists'})
-    return
-  }
-
-  // Generate an id and ensure it's not taken
+  // Generate an id and ensure it's unique
   let idTaken = false
-  let id = 0
+  let _id = 0
   do {
-    id = crypto.randomBytes(12).toString('hex')
+    _id = crypto.randomBytes(12).toString('hex')
 
     users.forEach(user => {
-      if(user['_id'] === id) {      
+      if(user['_id'] === _id) {      
         idTaken = true
         return
       }
@@ -47,126 +31,98 @@ app.post('/api/users', (req, res) => {
   } while(idTaken)
 
   // Create user's object
-  let user = {
-    'username': req.body['username'], 
-    '_id': id
+  const user = {
+    _id: _id,
+    username: req.body.username
   }
 
   users.push(user)
   res.json(user)
 })
 
-// Allow users to be searched and returned
-app.get('/api/users/:search?', (req, res) => {
-  const search = req.params.search
-
-  // Send all users if search term was omitted
-  if(search == undefined) {
-    res.json(users)
-    return
-  }
-
-  // Search username and _id fields for match
-  let userFound = false
-  users.forEach(user => {
-    if(user['_id'] === search || user['username'] === search) {
-      userFound = true
-      res.json(user)
-    }
-  })
-
-  if(!userFound)
-    res.json({'error': 'User not found'})
-})
-
-function getUserIndex(id) {
-  let userIndex = 0
-  while(userIndex < users.length && users[userIndex]['_id'] !== id) {
-    userIndex++
-  }
-
-  return userIndex
-}
-
 // Post an exercise for a user
 app.post('/api/users/:id/exercises', (req, res) => {
   const id = req.params.id
-  const description = req.body['description']
-  const duration = parseInt(req.body['duration'])
-  const date = (new Date(req.body['date'])).toDateString()
-
-  // Check if inputs are valid
-  if(Number.isNaN(duration)) {
-    res.json({'error': 'Duration must be a number'})
-    return
-  }
-  if(date === 'Invalid Date') {
-    res.json({'error': 'Date must be in format \'yyyy-mm-dd\''})
-    return
-  }
+  const {description, duration, date} = req.body
 
   // Find user
-  const userIndex = getUserIndex(id)
-  if(userIndex === users.length) {
-    res.json({'error': 'User not found'})
+  const user = users.find(u => u._id === id)
+  if(!user) {
+    res.send('Could not find user')
     return
   }
-  const username = users[userIndex]['username']
 
-  // Add exercise
+  // Create and save exercise object
   const exercise = {
-    'username': username,
-    '_id': id,
-    'description': description,
-    'duration': duration,
-    'date': date
+    _id: user._id,
+    username: user.username,
+    description: description,
+    duration: parseInt(duration),
+    date: date ? new Date(date) : new Date()
   }
-
   exercises.push(exercise)
-  res.json(exercise)
+
+  // Reply to user
+  res.json({
+    ...exercise,
+    date: exercise.date.toDateString()
+  })
+})
+
+// Allow users to be searched and returned
+app.get('/api/users/', (req, res) => {
+  if(users.length === 0)
+    res.send('No users')
+  else
+    res.json(users)
 })
 
 app.get('/api/users/:id/logs', (req, res) => {
   const id = req.params.id
-  const from = new Date(req.query.from)
-  const to = new Date(req.query.to)
-  const limit = req.query.limit
+  let {from, to, limit} = req.query
+
+  // Convert query args to dates
+  if(from)
+    from = new Date(from)
+  if(to)
+    to = new Date(to)
 
   // Find user
-  const userIndex = getUserIndex(id)
-  if(userIndex === users.length) {
-    res.json({'error': 'User not found'})
+  const user = users.find(u => u._id === id)
+  if(!user) {
+    res.send('Could not find user')
     return
   }
-  const username = users[userIndex]['username']
 
-  // Find exercises
-  let matchingExercises = []
-  exercises.forEach(exercise => {
-    const exerciseDate = (new Date(exercise['date']))
-    if(
-      exercise['_id'] === id && 
-      ((from != 'Invalid Date' && exerciseDate >= from) || 
-      from == 'Invalid Date') && 
-      ((to != 'Invalid Date' && exerciseDate <= to) || 
-      to == 'Invalid Date') && 
-      ((limit != undefined && 
-      matchingExercises.length < limit) ||
-      limit == undefined)
-    ) {
-      matchingExercises.push({
-        'description': exercise['description'],
-        'duration': exercise['duration'],
-        'date': exercise['date']
-      })
-    }
+  // Filter exercises
+  let matchingExercises = exercises.filter(e => {
+    return e._id === id && 
+    ((from && e.date.getTime() >= from.getTime()) || !from) && 
+    ((to && e.date.getTime() <= to.getTime()) || !to)
   })
 
-  res.json({
-    ...users[userIndex],
+  // Constrain to (limit) entries
+  if(limit && matchingExercises.length > limit)
+    matchingExercises = matchingExercises.slice(0, limit);
+
+  // Format filtered exercises for return
+  matchingExercises = matchingExercises.map(e => {return {
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }})
+
+  let result = {
+    ...user,
     count: matchingExercises.length,
     log: matchingExercises
-  })
+  }
+  if(from)
+    result['from'] = from.toDateString()
+  if(to)
+    result['to'] = to.toDateString()
+
+  res.json(result)
 })
 
 
